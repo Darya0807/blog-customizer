79eb7c517b35cd1f710ff6b3aaa6d85f3198b099
Darya Kiseleva <d.kissa25@mail.ru>
2026-02-18 22:34:45 +0300

    Реализация обновления и сохранения состояния страницы и формы, закрытия/открытия сайдбара

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
-import clsx from 'clsx';
+import { useState, StrictMode, CSSProperties, useCallback } from 'react';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +15,29 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [settings, setSettings] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleApplySettings = useCallback((newState: ArticleStateType) => {
+		setSettings(newState);
+	}, []);
+
 	return (
 		<main
-			className={clsx(styles.main)}
+			className={styles.main}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': settings.fontFamilyOption.value,
+					'--font-size': settings.fontSizeOption.value,
+					'--font-color': settings.fontColor.value,
+					'--container-width': settings.contentWidth.value,
+					'--bg-color': settings.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				onApply={handleApplySettings}
+				currentSettings={settings}
+			/>
 			<Article />
 		</main>
 	);
